35384148295980c153b7e3236f5f851f9625adde
Gouravmoy <gouravmoy.mohanty91@gmail.com>
2016-01-30 23:36:41 +0530

    Boot Strap Working Fine

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,18 +3,34 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-controller('AdminPagesCtrl', ['$scope', '$log', 'pagesFactory',
-    function ($scope, $log, pagesFactory) {
-        pagesFactory.getPages().then(
-            function (response) {
-                $scope.allPages = response.data;
-            },
-            function (err) {
-                $log.error(err);
-            });
+    controller('AdminPagesCtrl', ['$scope', '$log', 'pagesFactory',
+        function ($scope, $log, pagesFactory) {
+            pagesFactory.getPages().then(
+                function (response) {
+                    $scope.allPages = response.data;
+                },
+                function (err) {
+                    $log.error(err);
+                });
 
-        $scope.deletePage = function (id) {
-            pagesFactory.deletePage(id);
+            $scope.deletePage = function (id) {
+                pagesFactory.deletePage(id);
+            };
+        }
+    ])
+    .controller('AdminLoginCtrl', ['$scope', '$location', '$cookies', 'AuthService', '$log', function ($scope, $location, $cookies, AuthService, $log) {
+        $scope.credentials = {
+            username: '',
+            password: ''
         };
-    }
-]);
+        $scope.login = function (credentials) {
+            AuthService.login(credentials).then(
+                function (res, err) {
+                    cookies.loggedInUser = res.data
+                    $location.path('/admin/pages');
+                }, function (err) {
+                    $log.log(err);
+                }
+            );
+        }
+    }])
